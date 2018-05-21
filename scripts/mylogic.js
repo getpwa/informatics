442a362384fcafc22b2e8db625eae200ecbe527b
syamkrishna bg <bgskrishna1@gmail.com>
2018-05-22 04:21:09 +0530

    dev

--- scripts/mylogic.js
+++ scripts/mylogic.js
@@ -8,7 +8,7 @@
        $http({
         method: 'GET',
         url: 'https://getpwa.github.io/informatics/testdata.json'
-      }).success(function(data) {
+      }).then(function(data) {
        
         $scope.sources = data.sources; // response data
       });
